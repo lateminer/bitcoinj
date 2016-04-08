940bfdf984bf2cbaf917cf3ea6add80a7c887c5d
lateminer <late-miner@ya.ru>
2016-04-09 02:16:33 +0300

    Use Blackcoin payment protocol MIME types

--- core/src/main/java/org/bitcoinj/protocols/payments/PaymentProtocol.java
+++ core/src/main/java/org/bitcoinj/protocols/payments/PaymentProtocol.java
@@ -47,9 +47,9 @@ import java.util.List;
 public class PaymentProtocol {
 
     // MIME types as defined in BIP71.
-    public static final String MIMETYPE_PAYMENTREQUEST = "application/bitcoin-paymentrequest";
-    public static final String MIMETYPE_PAYMENT = "application/bitcoin-payment";
-    public static final String MIMETYPE_PAYMENTACK = "application/bitcoin-paymentack";
+    public static final String MIMETYPE_PAYMENTREQUEST = "application/blackcoin-paymentrequest";
+    public static final String MIMETYPE_PAYMENT = "application/blackcoin-payment";
+    public static final String MIMETYPE_PAYMENTACK = "application/blackcoin-paymentack";
 
     /**
      * Create a payment request with one standard pay to address output. You may want to sign the request using
